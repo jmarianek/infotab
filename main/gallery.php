44190ba26ea160785eae2b028238e7636aaa5a2b
Josef Marianek <josef.mariaanek@gmail.com>
2022-02-21 16:35:08 +0100

    chybejici soubory

---
+++ main/gallery.php
@@ -0,0 +1,95 @@
+<?php
+// TODO - nacist fotky z adresare
+?>
+
+
+<section>
+<img src="img/gallery/homepage-motiv-1920-01-b.JPG" style=" height: 100%" />
+<br/>
+<small>Budova gymnázia</small>
+</section>
+
+<section>
+<img src="img/gallery/IMG_3193.JPG" style="margin-top: 80px;" />
+<br/>
+<small>Best in English</small>
+</section>
+
+
+<section>
+<img src="img/gallery/DSCN0365.JPG" style="margin-top: 80px;" />
+<br/>
+<small>Kvarta učí tercii</small>
+</section>
+
+
+<section>
+<img src="img/gallery/DSCN0376.JPG" style="margin-top: 80px;" />
+<br/>
+<small>V chemické laboratoři</small>
+</section>
+
+
+<section>
+<img src="img/gallery/DSCN0380.JPG" style="margin-top: 80px;" />
+<br/>
+<small>Best in English</small>
+</section>
+
+
+<section>
+<img src="img/gallery/DSC01365.JPG" style="margin-top: 80px;" />
+<br/>
+<small>Školní výlet</small>
+</section>
+
+
+<section>
+<img src="img/gallery/IMG_2177.JPG" style="margin-top: 80px;" />
+<br/>
+<small>Maturita</small>
+</section>
+
+
+<section>
+<img src="img/gallery/index.JPG" style="margin-top: 80px;" />
+<br/>
+<small>Maturita</small>
+</section>
+
+
+<section style="height: 100%; overflow: none">
+<img src="img/gallery/P1010014.JPG" style="margin-top: 80px;" />
+<br/>
+<small>Sportovní aktivity</small>
+</section>
+
+
+<section style="height: 100%">
+<img src="img/gallery/P1010030.JPG" style="margin-top: 80px;" />
+<br/>
+<small>Sportovní aktivity</small>
+</section>
+
+
+<section>
+<img src="img/gallery/2014-05-16-sbory1.JPG" style="margin-top: 80px;" />
+<br/>
+<small>Sbor</small>
+</section>
+
+
+<section>
+<img src="img/gallery/2014-05-16-sbory2.JPG" style="margin-top: 80px;" />
+<br/>
+<small>Sbor</small>
+</section>
+
+
+<section>
+<img src="img/gallery/20211112_180704.JPG" style="margin-top: 80px;" />
+<br/>
+<small>Prodloužená</small>
+</section>
+
+
